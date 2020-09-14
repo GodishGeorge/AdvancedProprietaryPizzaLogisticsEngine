c85796b361c2d0735dc01135e31a6e6315153c1a
George Godish <GGodish@ashleyfurniture.com>
2020-09-14 16:31:06 -0400

    Revert "Init Push"
    This reverts commit 2cc96883e095a1100947b0e01c7be782e393e519.

--- AdvancedProprietaryPizzaLogisticsEngine/Program.cs
+++ AdvancedProprietaryPizzaLogisticsEngine/Program.cs
@@ -1,32 +1,12 @@
-﻿using AdvancedProprietaryPizzaLogisticsEngine.Models;
-using AdvancedProprietaryPizzaLogisticsEngine.Services;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
+﻿using System;
 
 namespace AdvancedProprietaryPizzaLogisticsEngine
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
-            var _pineappleLovers = new PineappleLovers();
-            var _engineersPizzaOrder = new EngineersPizzaOrder();
-            var _favortieToppings = new FavoriteToppings();
-
-            var jsonPath = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\netcoreapp3.1\Data.json"));           
-
-            var items = JsonConvert.DeserializeObject<List<Employee>>(jsonPath);
-
-            _pineappleLovers.PineapplePizza(items);
-
-            _engineersPizzaOrder.PizzaOrder(items);
-
-            _favortieToppings.FavoriteToppingsByDepartment(items);                      
-
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
