bb92e177f50323e238677e9d3418eba3f95c528b
George Godish <GGodish@ashleyfurniture.com>
2020-09-14 09:26:24 -0400

    Add project files.

---
+++ AdvancedProprietaryPizzaLogisticsEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedProprietaryPizzaLogisticsEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
